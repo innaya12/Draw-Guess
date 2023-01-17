ea249962c2afe2a69346466bdca33c9b56bd520c
innaya12 <yakubov.inna2@gmail.com>
2023-01-17 15:52:09 +0200

    the data is being sent from word component

--- drawnguess/src/context/GameContext.js
+++ drawnguess/src/context/GameContext.js
@@ -4,7 +4,7 @@ export const words = {
     easyWord: {value: '', points: 1},
     mediumWord: {value: '', points: 3},
     hardWord: {value: '', points: 5},
-    chosenWord: {value: '', level: ''}
+    chosenWord: {value: '', level: '', points: 0}
 }
 
 export const GameContext = createContext(words);
