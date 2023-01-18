db208fefecb6b72bc1e54e7b5f201f0a6a00fd79
innaya12 <yakubov.inna2@gmail.com>
2023-01-18 11:53:11 +0200

    Works!

--- drawnguess/src/components/Guessing.jsx
+++ drawnguess/src/components/Guessing.jsx
@@ -1,16 +1,47 @@
 import React from 'react';
+import { useState } from 'react';
 import { useContext } from 'react';
+import { PlayersContext } from '../context/GameContext'
 import { GameContext } from '../context/GameContext'
+import { useNavigate } from "react-router-dom";
 
 const Guessing = () => {
+  let guessingWord ;
+  const [result , setResult] = useState('')
+  let navigate = useNavigate();
+  const playersContext = useContext(PlayersContext);
   const gameContext = useContext(GameContext);
-  console.log('gameContext guessing', gameContext)
 
+  function handleChange(e){
+    console.log(' e.target.value',  e.target.value)
+    guessingWord = e.target.value;
+  }
+
+  function onClick(){
+    
+    if(guessingWord === gameContext.chosenWord.value){
+      setResult(`Yayy. You won! You have ${gameContext.chosenWord.points} points! Now its your turn to draw!`);
+      setTimeout(() => {
+        console.log('navigate to game');
+        navigate("/game");
+      }, 5000);
+      
+    } else {
+      setResult("Sorry.. you are wrong. Try again!");
+
+    }
+  }
+  console.log('gameContext.chosenWord.value', gameContext.chosenWord.value)
+  console.log('result', result)
   return (
     <div>
       <h1>Guessing</h1>
-      <p>{gameContext.value}</p>
-      <p>{gameContext.value}</p>
+      <img src={playersContext.saveImage} />
+      <input placeholder="what do you see?" name="name" onChange={handleChange} />
+      <button onClick={onClick}> Check </button>
+      {result && <p>{result}</p>}
+
+
     </div>
   );
 };
--- drawnguess/src/components/Word.jsx
+++ drawnguess/src/components/Word.jsx
@@ -5,14 +5,14 @@ import { GameContext } from '../context/GameContext'
 import Drawing from './Drawing';
 var randomWords = require('random-words');
 
-const wordArray = (randomWords({ exactly: 5, maxLength: 8}));
 /// getting 5 words > searching for the 3 words length needed >  choosing
 const Word = () => {
   const [play, setPlay] = useState(false);
   const gameContext = useContext(GameContext);
   console.log('gameContext word', gameContext)
-
+  
   useEffect(()=>{
+    const wordArray = (randomWords({ exactly: 5, maxLength: 8}));
     console.log(wordArray);
     wordArray.map(word =>{
       if(word.length === 3 || word.length === 4){
@@ -23,13 +23,13 @@ const Word = () => {
         gameContext.hardWord.value = word;
       }
     });
+  });
 
-  }, []);
 
-
-  function chooseWord(chosenWord){
-    console.log('onclick', chosenWord, gameContext[chosenWord].value, gameContext[chosenWord].points)
-    gameContext.chosenWord = { value: gameContext.chosenWord.value, level: chosenWord, points: gameContext[chosenWord].points }
+  function chooseWord(e, chosenWord){
+    console.log('check',chosenWord)
+    console.log('onclick', chosenWord, e.target.value, gameContext[e.target.value].points)
+    gameContext.chosenWord = { value: chosenWord, level: e.target.value, points: gameContext[e.target.value].points }
     setPlay(true);
     /// choosing a word, saving its data in context so it will pass to the drawing component
   }
@@ -38,9 +38,9 @@ const Word = () => {
     <div>
       <h1>choose a word</h1>
       <p>Please choose what you want to draw!</p>
-      <button value="easyWord" onClick={e => chooseWord(e.target.value)}>{gameContext.easyWord.value}</button>
-      <button value="mediumWord" onClick={e => chooseWord(e.target.value)}>{gameContext.mediumWord.value}</button>
-      <button value="hardWord" onClick={e => chooseWord(e.target.value)}>{gameContext.hardWord.value}</button>
+      <button value="easyWord" onClick={e => chooseWord(e, gameContext.easyWord.value)}>{gameContext.easyWord.value}</button>
+      <button value="mediumWord" onClick={e => chooseWord(e, gameContext.mediumWord.value)}>{gameContext.mediumWord.value}</button>
+      <button value="hardWord" onClick={e => chooseWord(e, gameContext.hardWord.value)}>{gameContext.hardWord.value}</button>
       { play && <Drawing/>}
     </div>
   );
